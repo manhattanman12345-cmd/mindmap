17304a40587431f5ae1aa5ef0159a29eb3754bcf
manhattanman12345-cmd <manhattanman12345@gmail.com>
2026-07-06 23:19:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+// キャッシュ優先の簡易 Service Worker (ビルドごとに更新)
+const C = "mindmap-v1783345926246";
+const ASSETS = ["./", "./index.html", "./assets/index-app.js", "./manifest.webmanifest", "./icon.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(C).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys()
+    .then((ks) => Promise.all(ks.filter((k) => k !== C).map((k) => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request, { ignoreSearch: true }).then((r) => r || fetch(e.request)));
+});
