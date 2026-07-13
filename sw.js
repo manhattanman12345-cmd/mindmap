55e6f74189b594b9f91c4eb4889e0f27f110eadb
manhattanman12345-cmd <manhattanman12345@gmail.com>
2026-07-13 18:15:10 +0900

    Refresh cache for flow edge insertion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // オンライン時は最新版、オフライン時はキャッシュを使用する Service Worker
-const C = "mindmap-1.38.0-1783925280646";
+const C = "mindmap-1.38.0-1783933781096";
 const ASSETS = ["./", "./index.html", "./index-app.js", "./manifest.webmanifest", "./icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(C).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
