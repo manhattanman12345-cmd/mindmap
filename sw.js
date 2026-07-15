e4055ab0d2831bf87f2abb0f7a81ed3a8d98e4e5
manhattanman12345-cmd <manhattanman12345@gmail.com>
2026-07-15 23:16:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // オンライン時は最新版、オフライン時はキャッシュを使用する Service Worker
-const C = "mindmap-1.39.0-1784078259534";
+const C = "mindmap-1.39.1-1784124510127";
 const ASSETS = ["./", "./index.html", "./index-app.js", "./manifest.webmanifest", "./icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(C).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
